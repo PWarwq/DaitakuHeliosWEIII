401f7c112f588a61664e98e49ae3964ae319f52c
PWarwq <vilux456@gmail.com>
2026-01-06 12:45:00 +0700

    secret v2

--- gambling.py
+++ gambling.py
@@ -2,24 +2,25 @@ import random
 import time
 
 print("Lesgo Gambling")
-
 time.sleep(2)
 
-ask = input("Lesgo gambling: [y]")
+ask = input("Lesgo gambling: [y] ")
 
-out = random.randint(1,100)
+if ask == "y":
+    print("=" * 20)
 
-if( ask == "y"):
-    print("="*20)
+    out = random.randint(1, 100)
+
+    if out == 67:
+        print("SIX SEVENNNNNNN")
+    elif out == 16:
+        print("This is magic number idk why")
+    elif out == 100:
+        print("YOU RICH")
+    else:
+        print("u Broke")
+
     print(f"The gambling Machine says your score is {out}")
 
-elif out == 67:
-    print('SIX SEVENNNNNNN')
-elif out == 16:
-    print("This is magic number idk why")
-    
-elif out == 100:
-    print("YOU RICH")
-    
 else:
-    print("u Broke")
+    print("No gambling today.")
